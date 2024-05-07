8d0f53c3145de8df155004153f133fb8c8860184
joao-zac <102718142+joao-zac@users.noreply.github.com>
2024-05-06 23:55:03 -0300

    :construction: Estruturação do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import './App.css'
 
+import backimg from  './assets/unsplash_Hcfwew744z4.svg' 
+import logo from "./assets/logo.svg"
+
 function App() {
 
   return (
     <main>
-      <h1>eFront</h1>
+
+      <div className='photo'>
+        <img src={backimg} alt="" />
+        <p>Aprenda desenvolvimento frontend do zero agora mesmo</p>
+      </div>
+
+      <div className='user'>
+
+        
+        <div className="presentation">
+
+          <img src={logo} alt="" />
+          <span>EFRONT</span>
+
+        </div>  
+        
+        
+        <form action="">
+
+          <h1>Faça seu login na plataforma</h1>
+
+          <input type="text" placeholder='E-mail'/>
+          <input type="text" placeholder='Senha'/>
+
+          <a href="https://www.google.com">Esqueci minha senha</a>
+
+          <button>Entrar</button>
+
+        </form>
+
+      </div>
+
     </main>
   )
 }
